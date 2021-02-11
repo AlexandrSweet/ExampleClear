27afd13c2ca4c9741e8a50cd999b4bee2ad4f3f8
td.tiara <td.tiara@live.com>
2021-02-11 13:57:06 +0200

    Add project files.

---
+++ ExampleClear/Index.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="ExampleClear.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lab" runat="server"></asp:Label>
+            <asp:Button  ID="dtn" runat="server" OnClick="dtn_Click"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ExampleClear/Index.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExampleClear
+{
+    public partial class Index : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (IsPostBack)
+            {
+                lab.Text = File.ReadAllText(Server.MapPath("~/app_data/TextFile.txt"));
+            }
+
+
+            Response.Redirect("Google.com");
+            
+
+            int count = 0;
+
+
+            object mycount = ViewState["count"];
+
+            if (mycount !=null)
+            {
+                count = (int)mycount;
+            }
+            count += 1;
+            ViewState["count"] = count;
+     
+           
+        }
+
+        protected void dtn_Click(object sender, EventArgs e)
+        {
+
+        
+            for (int i = 0; i < 10000; i++)
+            {
+                ViewState[i.ToString()] = i;
+            }
+        }
+    }
+}
